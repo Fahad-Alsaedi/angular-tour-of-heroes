12f8c029b62b7a0f418954efa588a6dc44bc5047
Fahad Alsaedi <fahad@Fahads-MacBook-Pro.local>
2021-08-05 16:46:09 +0300

    Display a selection list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { HEROES } from './mock-heroes';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+
+
+  
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,6 +10,8 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+  selectedHero?: Hero;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
@@ -17,5 +21,12 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
+
+  onSelect(hero: Hero): void {
+
+    this.selectedHero = hero;
+}
 
 }
