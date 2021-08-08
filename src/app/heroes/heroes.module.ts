ec24d706a5f2e7c754edbc34cf804f939466e733
Fahad Alsaedi <Alsaedi@Fahads-iMac.local>
2021-08-08 13:21:12 +0300

    adding the lazy loading modules

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { HeroesComponent } from "./heroes.component";
+
+@NgModule({
+    declarations:[
+        HeroesComponent,
+    ],
+    imports:[RouterModule,CommonModule],
+    exports:[
+        HeroesComponent,
+    ]
+    
+})
+export class HeroesModule{
+
+}
